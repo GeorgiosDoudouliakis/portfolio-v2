1ec909c07a7497f7b5aa0f161f678afbdc58e78a
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-04 22:15:38 +0300

    feat: Footer component implementation

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+      <footer className="w-full bg-zinc-900 py-3">
+          <p className="text-center text-white">Copyright &copy; 2022. All Rights Reserved.</p>
+      </footer>
+    );
+}
