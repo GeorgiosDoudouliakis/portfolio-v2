004056ee216d4bb267a47e2a187fdd1e135b995f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-05 00:52:53 +0300

    feat: Use a tags inside list items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -14,12 +14,24 @@ export default function Header() {
                 <div className="flex justify-center items-center w-10 h-10 bg-orange-500 text-lg text-white font-bold mr-6 rounded-sm">GD</div>
                 <nav className={ `${isOnMobile ? (!isMenuOpen && "hidden") : "initial" } absolute top-16 left-0 bg-zinc-900 w-full h-fit md:relative md:top-auto md:left-auto md:w-auto md:h-auto`}>
                     <ul className="flex flex-col md:flex-row">
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer hover:text-orange-500 md:py-2 md:mr-2" onClick={() => setMenuState(false)}>About</li>
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer hover:text-orange-500 md:py-2 md:mr-2" onClick={() => setMenuState(false)}>Education</li>
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer hover:text-orange-500 md:py-2 md:mr-2" onClick={() => setMenuState(false)}>Experience</li>
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer hover:text-orange-500 md:py-2 md:mr-2" onClick={() => setMenuState(false)}>Skills</li>
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer hover:text-orange-500 md:py-2 md:mr-2" onClick={() => setMenuState(false)}>Projects</li>
-                        <li className="text-center py-4 px-4 text-white rounded font-bold cursor-pointer transition-all hover:text-orange-500 md:py-2" onClick={() => setMenuState(false)}>Resume</li>
+                        <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
+                            <a href="#about" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">About</a>
+                        </li>
+                        <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
+                            <a href="#education" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">Education</a>
+                        </li>
+                        <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
+                            <a href="#experience" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">Experience</a>
+                        </li>
+                        <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
+                            <a href="#skills" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">Skills</a>
+                        </li>
+                        <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
+                            <a href="#projects" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">Projects</a>
+                        </li>
+                        <li className="text-center" onClick={() => setMenuState(false)}>
+                            <a href="#resume" className="inline-block text-white py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2">Resume</a>
+                        </li>
                     </ul>
                 </nav>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer text-white md:hidden" onClick={() => setMenuState(!isMenuOpen)}>
