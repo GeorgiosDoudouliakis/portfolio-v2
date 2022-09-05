9241e03898007ebb47d975d14af1540c0e53eded
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-06 00:02:34 +0300

    refactor: Change logo and logo/links font size

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,26 +11,26 @@ export default function Header() {
     return (
         <header className="w-full bg-zinc-900 py-3 px-6 relative md:px-0">
             <div className="container flex justify-between items-center mx-auto">
-                <div className="flex justify-center items-center w-10 h-10 bg-orange-500 text-lg text-white font-bold mr-6 rounded-sm">GD</div>
+                <span className="text-sm text-orange-500 font-bold p-2 lg:text-base">GeorgeDoudouliakis</span>
                 <nav className={ `${isOnMobile ? (!isMenuOpen && "hidden") : "initial" } absolute top-16 left-0 bg-zinc-900 w-full h-fit md:relative md:top-auto md:left-auto md:w-auto md:h-auto`}>
                     <ul className="flex flex-col md:flex-row">
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
-                            <a href="#about" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">About</a>
+                            <a href="#about" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">About</a>
                         </li>
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
-                            <a href="#education" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">Education</a>
+                            <a href="#education" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Education</a>
                         </li>
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
-                            <a href="#experience" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">Experience</a>
+                            <a href="#experience" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Experience</a>
                         </li>
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
-                            <a href="#skills" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">Skills</a>
+                            <a href="#skills" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Skills</a>
                         </li>
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
-                            <a href="#projects" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">Projects</a>
+                            <a href="#projects" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Projects</a>
                         </li>
                         <li className="text-center" onClick={() => setMenuState(false)}>
-                            <a href="#resume" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:py-2 md:w-auto">Resume</a>
+                            <a href="#resume" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Resume</a>
                         </li>
                     </ul>
                 </nav>
