0fac97828a5c04ac9dabddf8f6827497683bc03f
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-06 21:18:46 +0300

    refactor: Change footer font size

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="w-full bg-zinc-900 py-3">
-          <p className="text-center text-white">Copyright &copy; 2022. All Rights Reserved.</p>
+          <p className="text-sm text-center text-white lg:text-base">Copyright &copy; 2022. All Rights Reserved.</p>
       </footer>
     );
 }
