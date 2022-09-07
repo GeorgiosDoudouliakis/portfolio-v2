16613ddc01ca30a4bf51d3e4141f08cfc8368c0b
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-07 23:58:44 +0300

    refactor: Change nav close icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -42,7 +42,7 @@ export default function Header() {
                     ) :
                     (
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 cursor-pointer text-white md:hidden" onClick={() => setMenuState(!isMenuOpen)}>
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     )
                 }
