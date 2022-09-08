eada5cf6af2e5dd3fcf30bd7e04c9da1543d593d
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-08 22:28:57 +0300

    feat: Make resume link to download cv

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,7 +13,7 @@ export default function Header() {
             <div className="container flex justify-between items-center mx-auto">
                 <span className="text-md text-white font-bold p-2 border-2 border-orange-500 rounded-full lg:text-base">GD</span>
                 <nav className={ `${isOnMobile ? (!isMenuOpen && "hidden") : "initial" } absolute top-16 left-0 bg-zinc-900 w-full h-fit border-y-2 border-zinc-700 md:relative md:top-auto md:left-auto md:w-auto md:h-auto md:border-y-0`}>
-                    <ul className="flex flex-col md:flex-row">
+                    <ul className="flex flex-col py-4 md:flex-row md:py-0">
                         <li className="text-center md:mr-2" onClick={() => setMenuState(false)}>
                             <a href="#about" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">About</a>
                         </li>
@@ -24,7 +24,12 @@ export default function Header() {
                             <a href="#projects" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Projects</a>
                         </li>
                         <li className="text-center" onClick={() => setMenuState(false)}>
-                            <a href="#resume" className="inline-block text-white w-full py-4 px-4 font-bold cursor-pointer hover:text-orange-500 md:text-sm md:py-2 md:w-auto lg:text-base">Resume</a>
+                            <a href="/assets/pdf/DOUDOULIAKIS_GEORGIOS_CV.pdf" className="inline-flex border-2 border-orange-800 rounded text-orange-500 py-2 px-4 font-bold cursor-pointer hover:text-white hover:bg-orange-800 md:text-sm md:py-2 md:w-auto lg:text-base" download>
+                                <span className="mr-2">Resume</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                </svg>
+                            </a>
                         </li>
                     </ul>
                 </nav>
