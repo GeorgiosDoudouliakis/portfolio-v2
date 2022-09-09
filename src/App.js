3fdba66cede189fed2a2ea43bb9044273bd821cf
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-10 01:52:52 +0300

    feat: Find me on linkedin component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/header/Header';
 import Intro from './components/intro/Intro';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
+import FindMeOnLinkedin from "./components/find-me-on-linkedin/FindMeOnLinkedin";
 import Footer from './components/footer/Footer';
 import EmailWrapper from "./components/email-wrapper/EmailWrapper";
 import GoTop from "./components/go-top/GoTop";
@@ -15,6 +16,7 @@ function App() {
           <Intro/>
           <About/>
           <Contact/>
+          <FindMeOnLinkedin/>
           <Footer/>
           <div className="w-full h-1 bg-slate-500"></div>
 
