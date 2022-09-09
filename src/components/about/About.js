f91a316b845fbab81d96037e4a637b7f42ca3c9b
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-10 01:37:57 +0300

    refactor: Improve html and css

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,13 +4,13 @@ export default function About() {
     const technologies = ["HTML5", "CSS3", "JavaScript", "TypeScript", "SCSS", "Git", "Angular 2+", "Angular JS", "React (currently learning)"];
 
     return (
-      <motion.section id="about" className="py-10 px-6 bg-zinc-800 md:py-14 lg:py-16" initial={{ opacity: 0 }}
+      <motion.section id="about" className="text-center py-10 px-6 bg-zinc-800 md:py-14 lg:py-16" initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }} transition={{ duration: 0.6, delay: 1 }}>
-            <h2 className="text-center text-3xl mb-8 mx-auto">
+            <div className="flex justify-center text-3xl mb-8">
                 <span className="mr-3 text-slate-300">01.</span>
-                <span className="text-orange-500">About Me</span>
-            </h2>
-            <div className="text-center text-white mx-auto lg:w-3/5">
+                <h2 className="text-orange-500">About Me</h2>
+            </div>
+            <div className="text-white mx-auto lg:w-3/5">
                 <p className="mb-4 xl:text-lg">Hi! My name is George Doudouliakis and i am a Front End Web Developer located in Thessaloniki, GR. My enthusiasm for building websites brought me to the 'world' of <span className="text-slate-300">Front End Web Development</span> in early 2020. Started with simple websites and thereafter enhanced my  skills in <span className="text-slate-300">JavaScript</span>, i started learning <span className="text-slate-300">Angular 2+</span>.</p>
                 <p className="mb-4 xl:text-lg">I'm passionate about what i do, i love solving problems and learning every day something new or enhance my existing skills. I try to write clean code in the best possible way in order to achieve better performance and coding experience with respect to user experience (UX).</p>
                 <p className="mb-4 xl:text-lg">Technologies i use:</p>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,13 +2,13 @@ import {motion} from "framer-motion";
 
 export default function Contact() {
     return (
-        <motion.section id="contact" className="flex flex-col justify-center align-middle py-10 px-6 bg-zinc-900 md:py-14 md:px-0 lg:py-16" initial={{ opacity: 0 }}
+        <motion.section id="contact" className="text-center py-10 px-6 bg-zinc-900 md:py-14 md:px-0 lg:py-16" initial={{ opacity: 0 }}
                         whileInView={{ opacity: 1 }} transition={{ delay: 0.2 }} viewport={{ once: true }}>
-            <h2 className="text-center text-3xl mb-8">
+            <div className="flex justify-center text-3xl mb-8">
                 <span className="mr-3 text-slate-300">04.</span>
-                <span className="text-orange-500">Contact</span>
-            </h2>
-            <p className="text-center text-white w-80 mx-auto mb-8 md:w-96 xl:text-lg">If you want to discuss something you can text me anytime. I will try to respond as soon as possible!</p>
+                <h2 className="text-orange-500">Contact</h2>
+            </div>
+            <p className="text-white w-80 mx-auto mb-8 md:w-96 xl:text-lg">If you want to discuss something you can text me anytime. I will try to respond as soon as possible!</p>
             <a href="mailto: dudugeorge89@gmail.com" className="font-bold inline-flex text-orange-500 py-2 px-4 mx-auto cursor-pointer border-2 border-orange-800 rounded hover:text-white hover:bg-orange-800 md:text-sm md:py-2 lg:text-base">
                 <span className="mr-2">Talk to me</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mt-0.5">
