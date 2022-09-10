2798209e9b3c6bebf6127f5e183f8b4384c37577
Georgios Doudouliakis <dudugeorge89@gmail.com>
2022-09-10 14:17:21 +0300

    refactor: Remove animation on scroll and change styles

--- src/components/find-me-on-linkedin/FindMeOnLinkedin.js
+++ src/components/find-me-on-linkedin/FindMeOnLinkedin.js
@@ -1,9 +1,11 @@
-import {motion} from "framer-motion";
-
 export default function FindMeOnLinkedin() {
     return (
-        <motion.p className="text-center text-white py-6 bg-zinc-800" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.2 }} viewport={{ once: true }}>
-            Also you can find me on <a href="https://www.linkedin.com/in/georgios-doudouliakis-b97a5b19a/" target="_blank" rel="noreferrer" className="underline hover:text-orange-500">LinkedIn</a>
-        </motion.p>
+        <p className="text-center text-slate-300 py-1 bg-zinc-900">
+            You can catch
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="inline-block mx-2 mb-1 w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.05 4.575a1.575 1.575 0 10-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 013.15 0v1.5m-3.15 0l.075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 013.15 0V15M6.9 7.575a1.575 1.575 0 10-3.15 0v8.175a6.75 6.75 0 006.75 6.75h2.018a5.25 5.25 0 003.712-1.538l1.732-1.732a5.25 5.25 0 001.538-3.712l.003-2.024a.668.668 0 01.198-.471 1.575 1.575 0 10-2.228-2.228 3.818 3.818 0 00-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0116.35 15m.002 0h-.002" />
+            </svg>
+            me on <a href="https://www.linkedin.com/in/georgios-doudouliakis-b97a5b19a/" target="_blank" rel="noreferrer" className="underline hover:text-white">LinkedIn</a>
+        </p>
     )
 }
